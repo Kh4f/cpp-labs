d46f186bc4598951d11cdb94b823d46fe5d35a9a
Khaf <khafg050@gmail.com>
2023-09-14 07:00:55 +0300

    c3

--- Labwork-2/main.cpp
+++ Labwork-2/main.cpp
@@ -1,40 +1,96 @@
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//#include <sstream>
+//using namespace std;
+//
+//int main() {
+//
+//    vector<int> sequence;
+//    vector<int> uniqueNumbers;
+//
+//
+//    // Ввод элементов последовательности
+//    string inputString;
+//    cout << "Enter sequence elements separated by spaces: ";
+//    getline(cin, inputString);
+//    istringstream iss(inputString);
+//
+//    int number;
+//    while (iss >> number) {
+//        sequence.push_back(number);
+//    }
+//
+//
+//    // Сортировка последовательности
+//    sort(sequence.begin(), sequence.end());
+//
+//
+//    // Удаление повторяющихся элементов и формирование уникального массива
+//    uniqueNumbers.push_back(sequence[0]);
+//    for (int i = 1; i < sequence.size(); ++i) {
+//        if (sequence[i] != sequence[i - 1]) {
+//            uniqueNumbers.push_back(sequence[i]);
+//        }
+//    }
+//
+//
+//    // Вывод уникальных чисел
+//    cout << "Sequence of unique numbers: ";
+//    for (int num : uniqueNumbers) {
+//        cout << num << " ";
+//    }
+//    cout << endl;
+//}
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <sstream>
+using namespace std;
 
-int main() {
-    std::vector<int> sequence; // Вектор для хранения исходной последовательности
-    std::vector<int> uniqueNumbers; // Вектор для хранения уникальных чисел
+// Функция для ввода последовательности чисел
+vector<int> inputSequence() {
+    vector<int> sequence;
+    string inputString;
+    cout << "Enter sequence elements separated by spaces: ";
+    getline(cin, inputString);
+    istringstream iss(inputString);
 
-    int n; // Количество элементов в последовательности
-    std::cout << "Введите количество элементов в последовательности: ";
-    std::cin >> n;
-
-    // Ввод элементов последовательности
-    std::cout << "Введите элементы последовательности:\n";
-    for (int i = 0; i < n; ++i) {
-        int num;
-        std::cin >> num;
-        sequence.push_back(num);
+    int number;
+    while (iss >> number) {
+        sequence.push_back(number);
     }
+    return sequence;
+}
 
-    // Сортировка последовательности
-    std::sort(sequence.begin(), sequence.end());
-
-    // Удаление повторяющихся элементов и формирование уникального массива
-    uniqueNumbers.push_back(sequence[0]); // Первый элемент всегда уникален
-    for (int i = 1; i < n; ++i) {
+// Функция для удаления повторяющихся элементов и формирования уникального массива
+vector<int> removeDuplicates(const vector<int>& sequence) {
+    vector<int> uniqueNumbers;
+    uniqueNumbers.push_back(sequence[0]);
+    for (int i = 1; i < sequence.size(); ++i) {
         if (sequence[i] != sequence[i - 1]) {
             uniqueNumbers.push_back(sequence[i]);
         }
     }
-
-    // Вывод уникальных чисел
-    std::cout << "Уникальные числа в последовательности: ";
-    for (int num : uniqueNumbers) {
-        std::cout << num << " ";
-    }
-    std::cout << std::endl;
-
-    return 0;
+    return uniqueNumbers;
 }
+
+// Функция для вывода уникальных чисел
+void printUniqueNumbers(const vector<int>& uniqueNumbers) {
+    cout << "Sequence of unique numbers: ";
+    for (int num : uniqueNumbers) {
+        cout << num << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    vector<int> sequence = inputSequence();
+    sort(sequence.begin(), sequence.end());
+
+    vector<int> uniqueNumbers = removeDuplicates(sequence);
+
+    printUniqueNumbers(uniqueNumbers);
+}
+
